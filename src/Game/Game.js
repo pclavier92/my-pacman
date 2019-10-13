47f2a760efd5e7f10dd1d2b459fd7ccb7ffe8d27
Philippe Clavier <pclavier92@gmail.com>
2019-10-13 11:48:18 -0300

    Add pause to game

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -3,10 +3,25 @@ import React from 'react';
 import Square from '../Square';
 import './Game.css';
 
+// Settings
 const ROWNS = 10;
 const COLUMNS = 25;
-
 const GHOST_SPEED = 250;
+
+const EMPTY = {
+  isWalkable: true,
+  render: ''
+}
+
+const DOT = {
+  isWalkable: true,
+  render: 'dot'
+}
+
+const BIG_DOT = {
+  isWalkable: true,
+  render: 'bigdot'
+}
 
 const INITIAL_BOARD = [
   ['pacman-right', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'bigdot'],
@@ -21,15 +36,16 @@ const INITIAL_BOARD = [
   ['bigdot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'dot', 'ghost-left']
  ];
 
+// Config
 const MOVE_LEFT = 1;
 const MOVE_UP = 2;
 const MOVE_RIGHT = 3;
 const MOVE_DOWN = 4;
-
-const ARROW_LEFT = 'ArrowLeft';
-const ARROW_UP = 'ArrowUp';
-const ARROW_RIGHT = 'ArrowRight';
-const ARROW_DOWN = 'ArrowDown';
+const ARROW_LEFT_KEY = 'ArrowLeft';
+const ARROW_UP_KEY = 'ArrowUp';
+const ARROW_RIGHT_KEY = 'ArrowRight';
+const ARROW_DOWN_KEY = 'ArrowDown';
+const SPACE_KEY = ' '
 
 class Game extends React.Component {
   constructor(props) {
@@ -39,6 +55,7 @@ class Game extends React.Component {
       board: INITIAL_BOARD,
       isGameOver: false,
       isGameWon: false,
+      isGamePaused: false,
       pacman: {
         row: 0,
         column: 0
@@ -46,7 +63,7 @@ class Game extends React.Component {
       ghost: {
         row: ROWNS-1,
         column: COLUMNS-1,
-        previousState: 'bigdot'
+        previousSquare: 'bigdot'
       }
     }
     this.nextTurn = this.nextTurn.bind(this);
@@ -57,6 +74,17 @@ class Game extends React.Component {
   }
   componentWillUnmount() {
     clearInterval(this.interval);
+  }
+
+  pauseGame() {
+    let { isGamePaused } = this.state;
+    isGamePaused = !isGamePaused;
+    if (isGamePaused) {
+      clearInterval(this.interval);
+    } else {
+      this.interval = setInterval(this.nextTurn, GHOST_SPEED);
+    }
+    this.setState({ isGamePaused });
   }
 
   nextTurn() {
@@ -71,8 +99,8 @@ class Game extends React.Component {
     board.forEach( row => {
       if (row.includes('dot') || 
         row.includes('bigdot') || 
-        ghost.previousState === 'dot' || 
-        ghost.previousState === 'bigdot' 
+        ghost.previousSquare === 'dot' || 
+        ghost.previousSquare === 'bigdot' 
       ) {
         isGameWon = false;
       }
@@ -100,7 +128,6 @@ class Game extends React.Component {
     if ( pacman.column > ghost.column ) {
       posibleMoves.push(MOVE_RIGHT);
     }
-    debugger;
     return posibleMoves;
   }
 
@@ -108,8 +135,7 @@ class Game extends React.Component {
     let { board, ghost, pacman, isGameOver } = this.state;
     const ammountMoves = posibleMoves.length;
     const nextMove = posibleMoves[Math.floor(Math.random() * ammountMoves)];
-    const { previousState } = ghost;
-    debugger;
+    const { previousSquare } = ghost;
     switch(nextMove) {
       case MOVE_LEFT:
         if ( ghost.column === 0 ) {
@@ -117,9 +143,9 @@ class Game extends React.Component {
           this.moveGhost(posibleMoves);
           return;
         }
-        board[ghost.row][ghost.column] = previousState;
+        board[ghost.row][ghost.column] = previousSquare;
         ghost.column--;
-        ghost.previousState = board[ghost.row][ghost.column];
+        ghost.previousSquare = board[ghost.row][ghost.column];
         board[ghost.row][ghost.column] = 'ghost-left';
         break;
       case MOVE_UP:
@@ -128,9 +154,9 @@ class Game extends React.Component {
           this.moveGhost(posibleMoves);
           return;
         }
-        board[ghost.row][ghost.column] = previousState;
+        board[ghost.row][ghost.column] = previousSquare;
         ghost.row--;
-        ghost.previousState = board[ghost.row][ghost.column];
+        ghost.previousSquare = board[ghost.row][ghost.column];
         board[ghost.row][ghost.column] = 'ghost-right';
         break;
       case MOVE_RIGHT:
@@ -139,9 +165,9 @@ class Game extends React.Component {
           this.moveGhost(posibleMoves);
           return;
         }
-        board[ghost.row][ghost.column] = previousState;
+        board[ghost.row][ghost.column] = previousSquare;
         ghost.column++;
-        ghost.previousState = board[ghost.row][ghost.column];
+        ghost.previousSquare = board[ghost.row][ghost.column];
         board[ghost.row][ghost.column] = 'ghost-right';
         break;      
       case MOVE_DOWN:
@@ -150,9 +176,9 @@ class Game extends React.Component {
           this.moveGhost(posibleMoves);
           return;
         }
-        board[ghost.row][ghost.column] = previousState;
+        board[ghost.row][ghost.column] = previousSquare;
         ghost.row++;
-        ghost.previousState = board[ghost.row][ghost.column];
+        ghost.previousSquare = board[ghost.row][ghost.column];
         board[ghost.row][ghost.column] = 'ghost-left';
         break;
         default:
@@ -166,32 +192,37 @@ class Game extends React.Component {
   }
 
   movePacman(event) {
-    let { board, pacman, ghost, isGameOver } = this.state;
+    let { board, pacman, ghost, isGameOver, isGamePaused } = this.state;
     if (isGameOver) return;
-    switch(event.key) {
-      case ARROW_LEFT:
+    const { key } = event;
+    if (isGamePaused && key != SPACE_KEY) return;
+    switch(key) {
+      case ARROW_LEFT_KEY:
         if ( pacman.column === 0 ) return;
         board[pacman.row][pacman.column] = '';
         pacman.column--;
         board[pacman.row][pacman.column] = 'pacman-left';
         break;
-      case ARROW_UP:
+      case ARROW_UP_KEY:
         if ( pacman.row === 0 ) return;
         board[pacman.row][pacman.column] = '';
         pacman.row--;
         board[pacman.row][pacman.column] = 'pacman-up';
         break;
-      case ARROW_RIGHT:
+      case ARROW_RIGHT_KEY:
         if ( pacman.column === COLUMNS-1 ) return;
         board[pacman.row][pacman.column] = '';
         pacman.column++;
         board[pacman.row][pacman.column] = 'pacman-right';
         break;
-      case ARROW_DOWN:
+      case ARROW_DOWN_KEY:
         if ( pacman.row === ROWNS-1 ) return;
         board[pacman.row][pacman.column] = '';
         pacman.row++;
         board[pacman.row][pacman.column] = 'pacman-down';
+        break;
+      case SPACE_KEY:
+        this.pauseGame();
         break;
       default:
         break;
@@ -204,6 +235,7 @@ class Game extends React.Component {
   }
 
   render (){
+    const { isGameOver, isGameWon, isGamePaused } = this.state;
     const Board = this.state.board.map( row => (
         <div>
           { row.map(t => <Square type={t} /> ) }
@@ -215,17 +247,22 @@ class Game extends React.Component {
         <div className="board">
           { Board }
         </div>
-        <FinishModal isGameOver={this.state.isGameOver} isGameWon={this.state.isGameWon} />
+        <Modal 
+          isGameOver={isGameOver} 
+          isGameWon={isGameWon} 
+          isGamePaused={isGamePaused}
+          />
       </div>
     );
   } 
 }
 
-const FinishModal = ({isGameOver, isGameWon}) => (
-  <div style={isGameOver || isGameWon ? { display: 'block' } : {}} className="modal">
+const Modal = ({isGameOver, isGameWon, isGamePaused}) => (
+  <div style={isGameOver || isGameWon || isGamePaused ? { display: 'block' } : {}} className="modal">
     <div className="modal-content">
       { isGameOver ? <h1 className="game-over">GAME OVER</h1> : null }
       { isGameWon ? <h1 className="game-won">YOU HAVE WON!</h1> : null }
+      { isGamePaused ? <h1 className="game-won">PAUSE</h1> : null }
     </div>
   </div>
 )
